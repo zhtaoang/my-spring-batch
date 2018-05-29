d62d2e77f079fbbd1fded7dcf3ac1c70832c7bf7
Tao Zhang <zhangtao.todd@gmail.com>
2018-05-28 22:14:46 -0700

    first commit

---
+++ src/main/java/com/tao/myspringbatch/MySpringBatchApplication.java
@@ -0,0 +1,12 @@
+package com.tao.myspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBatchApplication.class, args);
+    }
+}
